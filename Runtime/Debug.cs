86fe52db5e877ec67b2aef0bef7f37a20fb5438d
장희용 <ralf1204@com2us.com>
2023-01-20 16:39:19 +0900

    [Release] 0.1.0

---
+++ Runtime/Debug.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Debug : MonoBehaviour
+{
+    public void Log(string message)
+    {
+        UnityEngine.Debug.Log(message);
+    }
+}
